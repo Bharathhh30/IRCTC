103bf84c8a3fe300039a77d3ebb7d5bca3df24ab
Bharathhh30 <vnsb30@gmail.com>
2025-07-22 19:09:14 +0530

    added getters and setters in Ticket.java

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -7,11 +7,86 @@ public class Ticket {
     private String userId;
     private String source;
     private String destination;
-    private Date dateOfTravel;
-    private  Train train;
+    private String dateOfTravel;
+    private Train train;
 
-//    return ticket info
-    public String getTicketInfo(){
-        return "";
+
+    // parameterised constructor
+    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train) {
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
     }
+
+    //    getters
+//    return ticket info
+    public String getTicketInfo() {
+        return String.format("Ticket Id: %s belongs to User %s from %s to %s on %s", ticketId, userId, source, destination, dateOfTravel);
+    }
+
+    //    getTicketId
+    public String getTicketId() {
+        return ticketId;
+    }
+
+//    getUserId
+    public String getUserId(){
+        return userId;
+    }
+
+//    getSource
+    public String getSource(){
+        return source;
+    }
+
+//    getDestination
+    public String getDestination(){
+        return destination;
+    }
+
+//    getDateOfTravel
+    public String getDateOfTravel(){
+        return  dateOfTravel;
+    }
+
+//    getTrain
+    public Train getTrain(){
+        return train;
+    }
+
+//    setters
+//    setTicketId
+    public void setTicketId(String ticketId){
+        this.ticketId = ticketId;
+    }
+
+//    setSource
+    public void setSource(String source){
+        this.source = source;
+    }
+
+//    setUserId
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+//    setDestination
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+
+//    setDateOfTravel
+    public void setDateOfTravel(String dateOfTravel){
+        this.dateOfTravel = dateOfTravel;
+    }
+
+//    setTrain
+    public void setTrain(Train train){
+        this.train = train;
+    }
+
+// End of function
 }
